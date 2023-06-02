dfc510510137d54ca771d71e5ff8159415873a94
hannah shaw <xiaohannwork@163.com>
2023-06-02 18:11:55 +0800

    fix: delete images lambda

--- Lambda/deleteImage.py
+++ Lambda/deleteImage.py
@@ -3,7 +3,9 @@ import boto3
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Attr
 
+
 def lambda_handler(event, context):
+    print(event)
     if event['httpMethod'] == 'DELETE':
         dynamodb = boto3.resource('dynamodb')
         table = dynamodb.Table('assignment2tags')
@@ -18,18 +20,39 @@ def lambda_handler(event, context):
                 print('No matching image found.')
                 return {
                     'statusCode': 400,
-                    'body': 'No matching image found.'
+                    'headers': {
+                        'Access-Control-Allow-Headers': 'Content-Type',
+                        'Access-Control-Allow-Methods': 'POST, DELETE',
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    'body': json.dumps({'message': 'No matching image found.'})
                 }
             else:
-                id = response['Items'][0]['id']  
-                responseDB = table.delete_item(Key={'id': id  })
-            
+                id = response['Items'][0]['id']
+                responseDB = table.delete_item(Key={'id': id})
+
             object_key = url.split('/')[-1]
             responseS3 = s3.Object('bucket-for-images-5225', object_key).delete()
             return {
                 'statusCode': 200,
-                'body': 'Successfully deleted.'
+                'headers': {
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Methods': 'POST, DELETE',
+                    'Access-Control-Allow-Origin': '*'
+                },
+                'body': json.dumps({'message': 'Successfully deleted.'})
             }
         except ClientError as e:
             print(e)
             return "error"
+    else:
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'POST, DELETE',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({'message': 'No matching image found.'})
+        }
+
