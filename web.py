2f526a6af0013a26233b032db286c19b3fa1ec49
hannah shaw <xiaohannwork@163.com>
2023-06-02 18:08:06 +0800

    feat:delete image and get Url by tags

--- web.py
+++ web.py
@@ -6,8 +6,17 @@ app = Flask(__name__)
 def begin():    
     return render_template('begin.html')
 
+@app.route('/getUrlsByTags')
+def test():
+    return render_template("getUrlsByTags.html")
+
+@app.route('/delete')
+def delete():
+    return render_template("delete.html")
+
 @app.route('/upload')
 def upload(): #跳转个页面在这里加一个，然后在html文件里把这个页面放到上面例如 href = "/upload"
     return render_template('upload.html')
+
 if __name__ == '__main__':
     app.run(debug=True,port=8080)
