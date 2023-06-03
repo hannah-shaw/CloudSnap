5d5084d0dd016c153bd813c047490987d7d7a7fc
czy101 <52922174+czy101@users.noreply.github.com>
2023-06-03 23:06:15 +0800

    Update Manual_addition_removal_tags.py
    新版的，对应的测试html是updata_tages_test.html

--- Lambda/Manual_addition_removal_tags.py
+++ Lambda/Manual_addition_removal_tags.py
@@ -1,52 +1,104 @@
-#这个lambda可以实现更新db中指定id图片的tags，可以用于tags的增删改查
-# This lambda can be implemented to update the tags of the specified id images in the db,
-# which can be used to add, delete, alter and remove tags
 import json
 import boto3
 from decimal import Decimal
-
 
 def decimal_default(obj):
     if isinstance(obj, Decimal):
         return float(obj)
     raise TypeError
-#DynamoDB 使用 Decimal 类型来存储数字，但是 Python 的 json 模块无法直接序列化此类型。
-#要解决此问题，您可以在序列化 JSON 之前将 Decimal 对象转换为可以序列化的类型，例如 float 或 int。
-# 您可以通过定义一个自定义函数来实现这一点，该函数将 Decimal 对象转换为 float，然后在调用 json.dumps() 时将其作为 default 参数传递
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('assignment2tags')
-
+    print("测试2247")
     print('Lambda function started')
     print(f'Event object: {event}')
-
-    data = json.loads(event['body'])
-    item_id = data['id']
+    
+    #data = json.loads(event['body'])#aws上测试的时候用这行
+    data = json.loads(json.loads(event['body'])['body'])  #使用浏览器html时候用这行
+    print("data数据",data)
+    print(data)
+    url = data['url']
+    type = data['type']
     tags = data['tags']
-    # item_id = event['id']
-    # tags = event['tags']
-    print("成功开始修改", id, tags)
+    
+    # 根据给定的 url 值查找对应的 id 值
+    response = table.scan(
+        FilterExpression='#u = :url',
+        ExpressionAttributeNames={
+            '#u': 's3-url'
+        },
+        ExpressionAttributeValues={
+            ':url': url
+        }
+    )
+    items = response.get('Items', [])
+    if not items:
+        return {
+            'statusCode': 404,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+            },
+            'body': json.dumps({'error': 'Item not found'})
+        }
+    
+    item_id = items[0]['id']
+    
+    # 获取当前图片的标签列表
+    current_item = table.get_item(Key={'id': item_id})
+    current_tags = current_item.get('Item', {}).get('tags', [])
+    
+    # 处理添加或删除标签的请求
+    if type == 1:
+        # 更新标签数量
+        for tag in tags:
+            tag_name = tag['tag']
+            tag_count = tag.get('count', 1)
+            
+            # 查找当前标签是否已存在
+            tag_exists = False
+            for current_tag in current_tags:
+                if current_tag[0] == tag_name:
+                    current_tag[1] = tag_count
+                    tag_exists = True
+                    break
+            
+            # 如果当前标签不存在，则添加新标签
+            if not tag_exists:
+                current_tags.append([tag_name, tag_count])
+    else:
+        # 删除标签
+        for tag in tags:
+            tag_name = tag['tag']
+            tag_count = tag.get('count', 1)
+            
+            # 查找并删除当前标签
+            for current_tag in current_tags:
+                if current_tag[0] == tag_name:
+                    current_tag[1] -= tag_count
+                    if current_tag[1] <= 0:
+                        current_tags.remove(current_tag)
+                    break
+    
+    # 更新 DynamoDB 表中的项目
     response1 = table.update_item(
         Key={
             'id': item_id
         },
         UpdateExpression='SET tags = :tags',
         ExpressionAttributeValues={
-            ':tags': tags
+            ':tags': current_tags
         }
     )
-    # 将更新后的条目返回
+    
+    # 返回更新后的项目
     updated_item = table.get_item(Key={'id': item_id})
     print("updated_item:", updated_item)
-
+     
     return {
         'statusCode': 200,
         'headers': {
-
             'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
         },
         'body': json.dumps(updated_item, default=decimal_default)
     }
-
-    # ,'Access-Control-Allow-Origin': "*"
