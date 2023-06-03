5fb11e8ba46108160b9edddeacb10daa6a52aff0
Huang Yuhang <HuangYH723@outlook.com>
2023-06-04 03:38:43 +0800

    Modify logic

--- web.py
+++ web.py
@@ -20,10 +20,13 @@ def test():
 @app.route('/delete')
 def delete():
     return render_template("delete.html")
+@app.route('/editPage')
+def editPage():
+    return render_template("editPage.html")
 
 @app.route('/upload')
 def upload(): #跳转个页面在这里加一个，然后在html文件里把这个页面放到上面例如 href = "/upload"
     return render_template('upload.html')
-
+    
 if __name__ == '__main__':
     app.run(debug=True,port=8080)
